f7889f03f6c0cc1ec24eb6a35572da33f5980a37
william vasquez <will.vasq77@gmail.com>
2025-06-17 14:26:38 -0400

    yes

--- gameSketch.js
+++ gameSketch.js
@@ -1,226 +1,505 @@
 let state = 0;
 let lives = 3;
 let score = 0;
+let currentRound = 1; // Track current round
 //loading images
-let playerImage, enemyImage, temp, player, bullet, bulletImage;
+let playerImage,
+  enemyImage,
+  temp,
+  player,
+  bullet,
+  bulletImage,
+  enemyBulletImage;
+let lastShotTime = 0; // Track when the last bullet was fired
+let shootCooldown = 400; // Cooldown in milliseconds (0.4 seconds)
 
 let groupAlien = [];
-
 let groupBullet = [];
+let groupEnemyBullet = []; // Array for enemy bullets
 
-function preload(){
-    
-    playerImage  = loadImage("images/player.png");
-    
-    enemyImage = loadImage("images/enemy.png");
-    
-    bulletImage= loadImage("images/bullet.png");
+function preload() {
+  playerImage = loadImage("images/player.png");
+
+  enemyImage = loadImage("images/enemy.png");
+
+  bulletImage = loadImage("images/bullet.png");
+  enemyBulletImage = loadImage("images/en-bullet.png");
 }
-// blueprints for character alien and bullet 
-class Bullet{ 
-   
-    constructor(x, y, image) {
-        
-        this.xPos = x;
-        
-        this.yPos = y;
-        
-        this.image = image;
-    }
+// blueprints for character alien and bullet
+class Bullet {
+  constructor(x, y, angle, image) {
+    this.xPos = x;
+    this.yPos = y;
+    this.image = image;
+    this.speed = 5; // Bullet speed
+    this.size = 50;
+
+    // Calculate direction based on angle, matching ship's rotation
+    this.directionX = cos(angle - PI / 2); // Subtract PI/2 to match ship's orientation
+    this.directionY = sin(angle - PI / 2);
+  }
+
+  move() {
+    this.xPos += this.directionX * this.speed;
+    this.yPos += this.directionY * this.speed;
+  }
+
+  draw() {
+    push();
+    translate(this.xPos + this.size / 2, this.yPos + this.size / 2);
+    rotate(atan2(this.directionY, this.directionX) + PI / 2); // Match ship's rotation
+    image(this.image, -this.size / 2, -this.size / 2, this.size, this.size);
+    pop();
+  }
+
+  isOffScreen() {
+    return (
+      this.xPos < -50 ||
+      this.xPos > width + 50 ||
+      this.yPos < -50 ||
+      this.yPos > height + 50
+    );
+  }
+
+  checkCollision(alien) {
+    return (
+      this.xPos < alien.xPos + alien.size &&
+      this.xPos + this.size > alien.xPos &&
+      this.yPos < alien.yPos + alien.size &&
+      this.yPos + this.size > alien.yPos
+    );
+  }
 }
 
+class Player {
+  constructor(x, y, image) {
+    this.xPos = x;
+    this.yPos = y;
+    this.image = image;
+    this.size = 50; // Size for collision detection
+    this.rotation = 0; // Add rotation property
+  }
 
-class Player{ 
-    
-    constructor(x, y, image) {
-        
-        this.xPos = x;
-        
-        this.yPos = y;
-        
-        this.image = image;
-    }
+  updateRotation() {
+    // Calculate angle between center of ship and mouse
+    let dx = mouseX - (this.xPos + this.size / 2);
+    let dy = mouseY - (this.yPos + this.size / 2);
+    // Add PI/2 to offset the initial upward orientation
+    this.rotation = atan2(dy, dx) + PI / 2;
+  }
+
+  draw() {
+    push(); // Save the current transformation state
+    translate(this.xPos + this.size / 2, this.yPos + this.size / 2); // Move to center of player
+    rotate(this.rotation); // Rotate
+    image(this.image, -this.size / 2, -this.size / 2, this.size, this.size); // Draw centered
+    pop(); // Restore the previous transformation state
+  }
+
+  getNosePosition() {
+    // Calculate the actual nose position after rotation
+    let noseX = this.xPos; // Left edge of ship
+    let noseY = this.yPos; // Top of ship
+    return { x: noseX, y: noseY };
+  }
 }
-     
 
 //Enemy blueprint
-class Alien{
-    
-    constructor(x, y, image){
-    
-        this.xPos = x;
-    
-        this.yPos = y;
-    
-        this.image = image;
+class Alien {
+  constructor(x, y, image) {
+    this.xPos = x;
+    this.yPos = y;
+    this.image = image;
+    this.speed = 2; // Base movement speed
+    this.size = 50; // Size of alien for collision detection
+    this.direction = {
+      x: random([-1, 1]), // Random initial x direction
+      y: random([-1, 1]), // Random initial y direction
+    };
+  }
+
+  move() {
+    // Randomly change direction occasionally
+    if (random() < 0.02) {
+      // 2% chance each frame to change direction
+      this.direction.x = random([-1, 1]);
+      this.direction.y = random([-1, 1]);
     }
+
+    // Update position
+    this.xPos += this.speed * this.direction.x;
+    this.yPos += this.speed * this.direction.y;
+
+    // Boundary checking
+    if (this.xPos < 0) {
+      this.xPos = 0;
+      this.direction.x *= -1;
+    }
+    if (this.xPos > width - 50) {
+      // 50 is the width of the enemy image
+      this.xPos = width - 50;
+      this.direction.x *= -1;
+    }
+    if (this.yPos < 0) {
+      this.yPos = 0;
+      this.direction.y *= -1;
+    }
+    if (this.yPos > height - 50) {
+      // 50 is the height of the enemy image
+      this.yPos = height - 50;
+      this.direction.y *= -1;
+    }
+  }
+}
+
+class EnemyBullet {
+  constructor(x, y, targetX, targetY, image) {
+    this.xPos = x;
+    this.yPos = y;
+    this.image = image;
+    this.speed = 2; // Slower speed for enemy bullets
+    this.size = 50;
+
+    // Calculate direction to player
+    let dx = targetX - x;
+    let dy = targetY - y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    // Prevent division by zero
+    if (distance === 0) {
+      this.directionX = 0;
+      this.directionY = 1;
+    } else {
+      this.directionX = dx / distance;
+      this.directionY = dy / distance;
+    }
+  }
+
+  move() {
+    this.xPos += this.directionX * this.speed;
+    this.yPos += this.directionY * this.speed;
+  }
+
+  isOffScreen() {
+    return (
+      this.xPos < -50 ||
+      this.xPos > width + 50 ||
+      this.yPos < -50 ||
+      this.yPos > height + 50
+    );
+  }
+
+  checkCollision(player) {
+    if (!player) return false;
+    return (
+      this.xPos < player.xPos + player.size &&
+      this.xPos + this.size > player.xPos &&
+      this.yPos < player.yPos + player.size &&
+      this.yPos + this.size > player.yPos
+    );
+  }
 }
 
 //Title screen and initilizing aliens
-function setup(){
-    
-    createCanvas(1000, 640);
-    
+function setup() {
+  createCanvas(1000, 640);
+
+  background(0);
+
+  player = new Player(250, 400, playerImage);
+
+  spawnAliens(); // Move alien spawning to a separate function
+}
+
+function spawnAliens() {
+  groupAlien = []; // Clear existing aliens
+  for (let i = 0; i < 5; i++) {
+    // Random x position between 0 and width-50 (accounting for enemy size)
+    let randomX = random(0, width - 50);
+    temp = new Alien(randomX, 20, enemyImage);
+    groupAlien.push(temp);
+  }
+}
+
+function draw() {
+  if (state == 0) {
     background(0);
-    
-    player = new Player(250, 400, playerImage);
+    fill(255, 255, 255);
+    textAlign(CENTER);
+    textSize(48);
+    text("GALAGA", width / 2, 100);
+    textSize(16);
+    textAlign(LEFT);
+    text("Your score:", 20, 20);
+    text(score, 120, 20);
+    textAlign(CENTER);
+    text("Round " + currentRound, width / 2, 20);
 
-    for(let i=0; i<5; i++){
-    
-        temp = new Alien(i*75, 20, enemyImage);
-    
-        groupAlien.push(temp);
-    
+    fill(0, 255, 0);
+    rect(width / 2 - 50, 190, 100, 50);
+    fill(0);
+    text("START", width / 2, 220);
+
+    // Draw the player with fixed rotation
+    if (player) {
+      player.draw();
     }
-    
+  }
+
+  if (state == 1) {
+    background(0);
+    fill(255, 255, 255);
+
+    text("Lives: " + lives, 950, 20);
+    text("Aliens: " + groupAlien.length, 20, 20);
+    text("Round " + currentRound, width / 2 - 30, 20);
+    text("Score: " + score, 20, 40);
+
+    if (player) {
+      player.updateRotation();
+      player.draw();
+    }
+
+    // Update and draw aliens
+    for (let i = 0; i < groupAlien.length; i++) {
+      if (!groupAlien[i]) continue;
+
+      image(
+        groupAlien[i].image,
+        groupAlien[i].xPos,
+        groupAlien[i].yPos,
+        50,
+        50
+      );
+      groupAlien[i].move();
+
+      // Random shooting for each alien - increased probability to 2%
+      if (random() < 0.02 && player) {
+        // 2% chance per frame to shoot
+        try {
+          let enemyBullet = new EnemyBullet(
+            groupAlien[i].xPos + 25, // Center of alien
+            groupAlien[i].yPos + 50, // Bottom of alien
+            player.xPos + 25, // Target center of player
+            player.yPos + 25,
+            enemyBulletImage
+          );
+          groupEnemyBullet.push(enemyBullet);
+          console.log(
+            "Enemy bullet created! Total bullets:",
+            groupEnemyBullet.length
+          );
+        } catch (error) {
+          console.log("Error creating enemy bullet:", error);
+        }
+      }
+
+      if (groupAlien[i].yPos > height) {
+        groupAlien[i].yPos = 0;
+        lives -= 1;
+        if (lives <= 0) {
+          state = 3;
+        }
+      }
+    }
+
+    if (groupAlien.length === 0) {
+      currentRound++;
+      spawnAliens();
+    }
+
+    if (keyIsDown(LEFT_ARROW)) {
+      player.xPos -= 5;
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) {
+      player.xPos += 5;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+      player.yPos += 5;
+    }
+
+    if (keyIsDown(UP_ARROW)) {
+      player.yPos -= 5;
+    }
+
+    // Handle player shooting
+    if (mouseIsPressed && mouseButton === LEFT) {
+      let currentTime = millis();
+      if (currentTime - lastShotTime >= shootCooldown) {
+        bullet = new Bullet(
+          player.xPos + player.size / 2 - 55, // 55 pixels left of center
+          player.yPos + player.size / 2, // Center of player
+          player.rotation, // Use player's rotation angle
+          bulletImage
+        );
+        groupBullet.push(bullet);
+        lastShotTime = currentTime;
+      }
+    }
+
+    // Update and draw bullets
+    for (let i = groupBullet.length - 1; i >= 0; i--) {
+      groupBullet[i].move();
+      groupBullet[i].draw();
+
+      // Check for collisions with aliens
+      for (let j = groupAlien.length - 1; j >= 0; j--) {
+        if (groupBullet[i].checkCollision(groupAlien[j])) {
+          // Remove both the bullet and the alien
+          groupBullet.splice(i, 1);
+          groupAlien.splice(j, 1);
+          score += 100; // Add points for hitting an alien
+          break; // Exit the alien loop since this bullet is now gone
+        }
+      }
+
+      // Remove bullets that are off screen
+      if (groupBullet[i] && groupBullet[i].isOffScreen()) {
+        groupBullet.splice(i, 1);
+      }
+    }
+  }
+
+  //stae 3 = game over screen
+  if (state == 3) {
+    background(0);
+    fill(255, 255, 255);
+    textSize(32);
+    textAlign(CENTER);
+    text("YOU LOSE", width / 2, height / 3);
+    textSize(24);
+    text("Final Score: " + score, width / 2, height / 2);
+    text("Rounds Completed: " + (currentRound - 1), width / 2, height / 2 + 40);
+
+    // Try again button
+    fill(0, 255, 0);
+    rect(width / 2 - 100, height / 2 + 80, 200, 50);
+    fill(0);
+    text("TRY AGAIN", width / 2, height / 2 + 110);
+
+    textAlign(LEFT); // Reset text alignment
+    textSize(16); // Reset text size
+  }
+  //leaderboards
+  if (state == 4) {
+  }
+
+  // Update and draw enemy bullets
+  for (let i = groupEnemyBullet.length - 1; i >= 0; i--) {
+    if (!groupEnemyBullet[i]) {
+      console.log("Invalid bullet found at index:", i);
+      groupEnemyBullet.splice(i, 1);
+      continue;
+    }
+
+    try {
+      groupEnemyBullet[i].move();
+      image(
+        groupEnemyBullet[i].image,
+        groupEnemyBullet[i].xPos,
+        groupEnemyBullet[i].yPos,
+        50,
+        50
+      );
+
+      // Check collision with player
+      if (player && groupEnemyBullet[i].checkCollision(player)) {
+        lives--;
+        groupEnemyBullet.splice(i, 1);
+        console.log("Player hit! Lives remaining:", lives);
+        if (lives <= 0) {
+          state = 3;
+        }
+        continue;
+      }
+
+      // Remove bullets that are off screen
+      if (groupEnemyBullet[i].isOffScreen()) {
+        groupEnemyBullet.splice(i, 1);
+        console.log(
+          "Bullet removed (off screen). Remaining bullets:",
+          groupEnemyBullet.length
+        );
+      }
+    } catch (error) {
+      console.log("Error updating enemy bullet:", error);
+      groupEnemyBullet.splice(i, 1);
+    }
+  }
 }
 
-function draw(){
-    if(state==0){
-        fill(255, 255, 255);
-    
-        text("GALAGA", 210, 100);
-    
-        text("Your score", 20, 20);
-        
-        text(score, 80,20);
+function mouseClicked() {
+  //back to game
+  if (
+    state == 3 &&
+    mouseX > width / 2 - 100 &&
+    mouseX < width / 2 + 100 &&
+    mouseY > height / 2 + 80 &&
+    mouseY < height / 2 + 130
+  ) {
+    background(0);
+    state = 1;
+    lives = 3;
+    currentRound = 1;
+    score = 0;
+    groupEnemyBullet = []; // Clear enemy bullets
+    spawnAliens();
+  }
 
-        fill(0, 255, 0);
-    
-        rect(185, 190, 100, 50);
-    
-        fill(0);
-    
-        text("START", 215, 220);
-
-    }
-   
-    if(state==1){
-    
-        background(0);
-        
-        fill(255,255,255);
-
-        text("lives", 950, 20);
-
-        text(lives, 980,20);
-
-        image(player.image, player.xPos, player.yPos, 50, 50);
-    
-        for(let i=0; i<groupAlien.length; i++){
-    
-            image(groupAlien[i].image, groupAlien[i].xPos, groupAlien[i].yPos, 50, 50);
-    
-            groupAlien[i].yPos += 3;
-            console.log(groupAlien.length);
-           
-            if(groupAlien[i].yPos>650){ 
-     
-                groupAlien[i].yPos=0;
-     
-                lives-=1;
-                
-                console.log(lives);
-                return lives;
-            }  
-            
-        }
-        if(lives<=0){
-           
-            background(0);
-            state=3;
-           
-        }
-        if(keyIsDown(LEFT_ARROW)){
-
-            player.xPos -= 5;   
-
-        }
-
-        if(keyIsDown(RIGHT_ARROW)){
-
-            player.xPos += 5;   
-
-        }
-
-        if(keyIsDown(DOWN_ARROW)){
-
-            player.yPos += 5;   
-
-        }
-
-        if(keyIsDown(UP_ARROW)){
-
-            player.yPos -= 5;
-
-        }
-    
-
-        if(keyIsDown(32)){
-
-            for(let i=0; i<1; i++){
-
-                bullet = new Bullet(player.xPos,player.yPos,bulletImage);
-
-                groupBullet.push(bullet);
-            }
-
-                for(let i=0; i<groupBullet.length; i++){
-
-                    image(groupBullet[i].image, groupBullet[i].xPos, groupBullet[i].yPos, 50, 50);
-
-                    groupBullet[i].yPos -= 3;
-                }
-
-        }
-    }
-
-
-//stae 3 = game over screen
-    if (state == 3){
-        fill(0);
-      
-        rect (200,300,100,100);
-        rect(200,400,100,100);
-        fill(255, 255, 255);
-        
-        text("try again",240,350);
-        
-        text("main menu",240,450);
-        
-        text('GAME OVER',200,100);
-        
-        text(score, 200, 200);
-
-    }
-    //leaderboards
-    if(state == 4){}
-}
-
-
-
-function mouseClicked(){
-    //back to game 
-
-    if ((state == 3)&&mouseX > 200 && mouseX < 300 && mouseY>300 &&mouseY <400){
-background(0);
-        state = 1;
-        lives = 3;
-  
-}
-
-if((state==3)&&mouseX>200&&mouseX<300&&mouseY>400&&mouseY<500){
+  if (
+    state == 3 &&
+    mouseX > 200 &&
+    mouseX < 300 &&
+    mouseY > 400 &&
+    mouseY < 500
+  ) {
     background(0);
     state = 0;
-    
+    currentRound = 1;
+    score = 0;
+    groupEnemyBullet = []; // Clear enemy bullets
+  }
 
-}
-
-//click START button to begin game
-if((state==0)&&(mouseX>=185)&&(mouseX<=285)&&(mouseY>=190)&&(mouseY<=240)){
-
+  //click START button to begin game
+  if (
+    state == 0 &&
+    mouseX >= width / 2 - 50 &&
+    mouseX <= width / 2 + 50 &&
+    mouseY >= 190 &&
+    mouseY <= 240
+  ) {
     background(0);
     lives = 3;
+    currentRound = 1;
+    score = 0;
+    groupEnemyBullet = []; // Clear enemy bullets
     state = 1;
+    spawnAliens();
+  }
 }
+
+function resetGame() {
+  // Clear all bullets
+  groupBullet = [];
+  // Reset player position and rotation
+  player = new Player(250, 400, playerImage);
+  // Clear all aliens
+  groupAlien = [];
+  // Reset score
+  score = 0;
+  // Reset game state
+  state = 0;
+  // Reset last shot time
+  lastShotTime = 0;
+}
+
+function mousePressed() {
+  if (state === 0) {
+    state = 1;
+    resetGame();
+  } else if (state === 3) {
+    state = 0;
+    resetGame();
+  }
 }
--- script.js
+++ script.js
@@ -1,150 +1,147 @@
-// //object literals
-// const blogs=[
-// {title:"why cheese", likes: 10},
-// {title:'10 reasons why', likes:50},
+// // //object literals
+// // const blogs=[
+// // {title:"why cheese", likes: 10},
+// // {title:'10 reasons why', likes:50},
 
-// ];
-// let user={
-// //   key:value
-//     name:'crystal',
-//     age:30,
-//     email:'fuck',
-//     local:"berlin",
-//     blogs:blogs,
-//     login(){
-//         console.log('user is racist');
-//     },
-//     logout(){
-//         console.log("user is out");
-//     },
-//     logblogs(){
-//         console.log("the blogs this person has person has wrote are:");
-//         this.blogs.forEach(blog=>{console.log(`The title is ${blog.title} and the amount of likes are ${blog.likes}`);
-//         })
-//     },
+// // ];
+// // let user={
+// // //   key:value
+// //     name:'crystal',
+// //     age:30,
+// //     email:'fuck',
+// //     local:"berlin",
+// //     blogs:blogs,
+// //     login(){
+// //         console.log('user is racist');
+// //     },
+// //     logout(){
+// //         console.log("user is out");
+// //     },
+// //     logblogs(){
+// //         console.log("the blogs this person has person has wrote are:");
+// //         this.blogs.forEach(blog=>{console.log(`The title is ${blog.title} and the amount of likes are ${blog.likes}`);
+// //         })
+// //     },
+// // };
+// //classes
+
+// // class User{
+// //     constructor(email,name){
+// //         this.email=email;
+// //         this.name=name;
+// //         this.score=0;
+// //     }
+
+// //     login(){
+// //         console.log(`${this.email} just logged in`)
+// //         return this;
+
+// //     }
+
+// //     logout(){
+// //         console.log(`${this.email} just logged out`)
+// //         return this;
+
+// //     }
+// //     updateScore(){
+// //         this.score++;
+// //         console.log(`${this.email} score is now ${this.score}`);
+// //         return this;
+// //     }
+
+// // }
+
+// // //class inheritence
+// // //will take constructors values and methods from what it extends from
+// // //what ever is inside the exteneded class can not not be used by original class
+// // class Admin extends User{
+
+// //     deleteUser(user){
+// //         users= users.filter(u=>{
+// //             return u.email != user.email;
+// //         })
+// //     }
+// // }
+
+// // let userOne = new User('fuck@racist.com','ryu');
+// // let userTwo = new User('fuck@racist.org','yosh');
+// // let admin= new Admin('race@g', 'saun')
+
+// // let users= [userOne, userTwo, admin];
+// // userOne.login();
+// // userTwo.logout();
+// // //method chaining only works and will be defined if the instance is returned
+// // userOne.login().updateScore().updateScore().logout();
+// // admin.deleteUser(userTwo)
+// // console.log(users);
+
+
+// // //dot notation
+
+// // // this.age=35;
+// // // //could be used with brackets but need to be in quotes
+// // // //or used dot notation
+// // // //could make a value into a var then quotes no needed
+// // // user.logblogs();
+// // // let key = 'age';
+// // // console.log(this[key])
+// // // console.log(this['age'])
+// // // //methods
+
+// // // user.login();
+// // // //Math is an object and date
+
+// // console.log(Math);
+// // console.log(Math.PI);
+// // console.log(Math.E);
+// // let area = 7.7;
+
+// // console.log(Math.round(area));
+// // console.log(Math.floor(area));
+// // console.log(Math.ceil(area));
+// // console.log(Math.trunc(area));
+
+// // let ran = Math.random()*100;
+// // console.log(ran);
+
+// // //primiteve data types
+// // //numbers,strings,bool,null,undefined,symbols
+
+// // //reference types
+// // //all types of objects,object literals,arrays,function,dates,all other objects
+
+
+// //prototypes
+
+// function User(email, name){
+//     this.email=email;
+//     this.name = name;
+//     this.online = false;
 // };
-//classes
+// //inheritance
+// //args the same as email and name
+// //User.apply gives the context to Admin
+// function Admin(...args){
+//    User.apply(this, args);
+// };
+// //now inherited anything from a different prototype 
 
-// class User{
-//     constructor(email,name){
-//         this.email=email;
-//         this.name=name;
-//         this.score=0;
-//     }
-
-//     login(){
-//         console.log(`${this.email} just logged in`)
-//         return this;
-
-//     }
-
-//     logout(){
-//         console.log(`${this.email} just logged out`)
-//         return this;
-
-//     }
-//     updateScore(){
-//         this.score++;
-//         console.log(`${this.email} score is now ${this.score}`);
-//         return this;
-//     }
-
+// Admin.prototype = Object.create( User.prototype);
+// //didn't do because the same as the last one 
+// Admin.prototype.deleteUser= function(){
+// console.log("e")
 // }
-
-// //class inheritence
-// //will take constructors values and methods from what it extends from
-// //what ever is inside the exteneded class can not not be used by original class
-// class Admin extends User{
-
-//     deleteUser(user){
-//         users= users.filter(u=>{
-//             return u.email != user.email;
-//         })
-//     }
-// }
-
-// let userOne = new User('fuck@racist.com','ryu');
-// let userTwo = new User('fuck@racist.org','yosh');
-// let admin= new Admin('race@g', 'saun')
 
 // let users= [userOne, userTwo, admin];
-// userOne.login();
-// userTwo.logout();
-// //method chaining only works and will be defined if the instance is returned
-// userOne.login().updateScore().updateScore().logout();
-// admin.deleteUser(userTwo)
-// console.log(users);
+// //class and prototypes are seperate
+// //this prototype will only be seen in __proto__ not class
+// //can still be used in class
+// User.prototype.login = function(){
+//     this.online= true;
+//     console.log(`${this.email} has logged in`);
+// }
+// User.prototype.logout = function(){
+//     this.online= false;
+//     console.log(`${this.email} has logged in`);
+// }
 
-
-// //dot notation
-
-// // this.age=35;
-// // //could be used with brackets but need to be in quotes
-// // //or used dot notation
-// // //could make a value into a var then quotes no needed
-// // user.logblogs();
-// // let key = 'age';
-// // console.log(this[key])
-// // console.log(this['age'])
-// // //methods
-
-// // user.login();
-// // //Math is an object and date
-
-// console.log(Math);
-// console.log(Math.PI);
-// console.log(Math.E);
-// let area = 7.7;
-
-// console.log(Math.round(area));
-// console.log(Math.floor(area));
-// console.log(Math.ceil(area));
-// console.log(Math.trunc(area));
-
-// let ran = Math.random()*100;
-// console.log(ran);
-
-// //primiteve data types
-// //numbers,strings,bool,null,undefined,symbols
-
-// //reference types
-// //all types of objects,object literals,arrays,function,dates,all other objects
-
-
-//prototypes
-
-function User(email, name){
-    this.email=email;
-    this.name = name;
-    this.online = false;
-};
-//inheritance
-//args the same as email and name
-//User.apply gives the context to Admin
-function Admin(...args){
-   User.apply(this, args);
-};
-//now inherited anything from a different prototype 
-
-Admin.prototype = Object.create( User.prototype);
-//didn't do because the same as the last one 
-Admin.prototype.deleteUser= function(){
-console.log("e")
-}
-
-let users= [userOne, userTwo, admin];
-//class and prototypes are seperate
-//this prototype will only be seen in __proto__ not class
-//can still be used in class
-User.prototype.login = function(){
-    this.online= true;
-    console.log(`${this.email} has logged in`);
-}
-User.prototype.logout = function(){
-    this.online= false;
-    console.log(`${this.email} has logged in`);
-}
-
-let userOne = new User('fuck@racist.com','ryu');
-let userTwo = new User('fuck@racist.org','yosh');
-let admin = new Admmin('odad','so')
